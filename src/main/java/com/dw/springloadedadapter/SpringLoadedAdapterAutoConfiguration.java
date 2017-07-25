45f8f4b140aa1372e3c2dfdc2609f8565ae26af5
Jaydeep Kumbhani <jaydeep.kumbhani@dreamworldsolutions.com>
2017-07-25 11:03:45 +0530

    added basic skeleton for filter and service component

--- src/main/java/com/dw/springloadedadapter/SpringLoadedAdapterAutoConfiguration.java
+++ src/main/java/com/dw/springloadedadapter/SpringLoadedAdapterAutoConfiguration.java
@@ -1,8 +1,14 @@
 package com.dw.springloadedadapter;
 
+import com.dw.springloadedadapter.filter.SpringLoadedAdapterFilter;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,12 +21,36 @@ public class SpringLoadedAdapterAutoConfiguration {
   private static final Logger logger =
       LoggerFactory.getLogger(SpringLoadedAdapterAutoConfiguration.class);
 
+  @Autowired
+  private AutowireCapableBeanFactory beanFactory;
+
   /**
-   * Log spring loaded adapter library successfully initialized.
+   * Spring loaded adapter library successfully initialized.
    * 
    */
   public SpringLoadedAdapterAutoConfiguration() {
     logger.info("SpringLoadedAdapterAutoConfiguration() :: "
         + "Initialization of Spring Loadded Adapter...");
+  }
+
+  /**
+   * Instantiates {@link SpringLoadedAdapterFilter} instance to add,replace or delete file on given
+   * path and with files . Filter is invoked for '/spring-loaded/*' url patterns and should have
+   * highest precedence.
+   * 
+   * @return {@link SpringLoadedAdapterFilter} instance.
+   */
+  @Bean
+  public FilterRegistrationBean companyContextFilter() {
+    logger.debug("companyContextFilter() :: Initializing Spring loaded filter bean.....");
+
+    SpringLoadedAdapterFilter springLoadedAdapterFilter = new SpringLoadedAdapterFilter();
+    beanFactory.autowireBean(springLoadedAdapterFilter);
+
+    FilterRegistrationBean filter = new FilterRegistrationBean(springLoadedAdapterFilter);
+    filter.setFilter(springLoadedAdapterFilter);
+    filter.addUrlPatterns("/spring-loaded/*");
+    filter.setOrder(1);
+    return filter;
   }
 }
---
+++ src/main/java/com/dw/springloadedadapter/filter/SpringLoadedAdapterFilter.java
@@ -0,0 +1,30 @@
+package com.dw.springloadedadapter.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * {@link SpringLoadedAdapterFilter} instance to add,replace or delete file on given path and with
+ * files . Filter is invoked for '/spring-loaded/*' url patterns and should have highest precedence.
+ * 
+ * @author Jaydeep Kumbhani
+ *
+ */
+public class SpringLoadedAdapterFilter extends GenericFilterBean {
+
+  private static final Logger logger = LoggerFactory.getLogger(SpringLoadedAdapterFilter.class);
+
+  public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+      FilterChain filterChain) throws IOException, ServletException {
+    // TODO add, replace or delete updated files
+
+  }
+}
