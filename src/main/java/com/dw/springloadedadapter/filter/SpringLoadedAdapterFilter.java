8a709370db5bbd9f20ebf97f1ee73e54f4158e5c
Jaydeep Kumbhani <jaydeep.kumbhani@dreamworldsolutions.com>
2017-07-25 18:18:59 +0530

    Implementation of filter

--- src/main/java/com/dw/springloadedadapter/filter/SpringLoadedAdapterFilter.java
+++ src/main/java/com/dw/springloadedadapter/filter/SpringLoadedAdapterFilter.java
@@ -10,6 +10,8 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * {@link SpringLoadedAdapterFilter} instance to add,replace or delete file on given path and with
@@ -22,9 +24,19 @@ public class SpringLoadedAdapterFilter extends GenericFilterBean {
 
   private static final Logger logger = LoggerFactory.getLogger(SpringLoadedAdapterFilter.class);
 
+  /**
+   * this filter invoke before security filter and for url "/spring-loaded/*" always given 200 OK.
+   * 
+   */
   public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
       FilterChain filterChain) throws IOException, ServletException {
-    // TODO add, replace or delete updated files
+    logger.debug("doFilter() :: Initialing filter");
+
+    HttpServletRequest request = (HttpServletRequest) servletRequest;
+    HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+    response.setStatus(200);
+    response.getWriter().write("{\r\n  \"code\":200,\r\n  \"message\":\"Success\"\r\n}");
 
   }
 }
